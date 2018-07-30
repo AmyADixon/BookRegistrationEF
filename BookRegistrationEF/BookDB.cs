ae18d8a87bb547d2863f3df8a84694c0ed54377e
AmyADixon <amysha.dixon@gmail.com>
2018-07-30 14:53:27 -0700

    Book update and delete

--- BookRegistrationEF/BookDB.cs
+++ BookRegistrationEF/BookDB.cs
@@ -61,5 +61,38 @@ namespace BookRegistrationEF {
             // Save changes
             context.SaveChanges();
         }
+
+        /// <summary>
+        ///  Allows the user to remove a book from the database
+        /// </summary>
+        /// <param name="novel"> The book to be removed </param>
+        public static void Delete(Book novel) {
+            var context = new BookContext();
+
+            // Add book to the context
+            context.Book.Add(novel);
+
+            // Mark book as deleted
+            context.Entry(novel).State = EntityState.Deleted;
+
+            // Sends query and saves changes
+            context.SaveChanges();
+        }
+
+        /* Uses Connected Scenario where the db context tracks the entities in memory */
+        public static void Delete(string isbn) {
+            var context = new BookContext();
+
+            // Select the book out of the database
+            // by searching with primary key;
+            // entity framework will track it
+            Book deletedBook = context.Book.Find(isbn);
+
+            // Remove book, marks book as deleted
+            context.Book.Remove(deletedBook);
+
+            // Sends delete quary to database and saves changes
+            context.SaveChanges();
+        }
     }
 }
